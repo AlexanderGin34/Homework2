ae8072f31ac6533c8c66cf149b0e878c322a9c99
AlexGin <Meinepostkam@gmail.com>
2021-11-05 01:40:41 +0300

    Add RGBA changes

--- Homework2/ViewController.swift
+++ Homework2/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ViewColors: UIView!
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    private let alphaValue = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: labels
+        labelRed.text = String(0)
+        labelGreen.text = String(0)
+        labelBlue.text = String(0)
+        
+        // MARK: sliders
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 255
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 255
+        sliderBlue.minimumValue = 0
+        sliderBlue.maximumValue = 255
+        
     }
 
-
+    @IBAction func sliderRedAction() {
+        labelRed.text = String(Int(sliderRed.value))
+        ViewColors.backgroundColor = UIColor(red: CGFloat(Int(labelRed.text!)!)/255, green: CGFloat(Int(labelGreen.text!)!)/255, blue: CGFloat(Int(labelBlue.text!)!)/255, alpha: 1)
+    
+    }
+        
+    @IBAction func sliderGreenAction() {
+        labelGreen.text = String(Int(sliderGreen.value))
+        ViewColors.backgroundColor = UIColor(red: CGFloat(Int(labelRed.text!)!)/255, green: CGFloat(Int(labelGreen.text!)!)/255, blue: CGFloat(Int(labelBlue.text!)!)/255, alpha: 1)
+        
+    }
+    
+    @IBAction func sliderBlueAction() {
+        labelBlue.text = String(Int(sliderBlue.value))
+        ViewColors.backgroundColor = UIColor(red: CGFloat(Int(labelRed.text!)!)/255, green: CGFloat(Int(labelGreen.text!)!)/255, blue: CGFloat(Int(labelBlue.text!)!)/255, alpha: 1)
+    }
+    
+    
+    
 }
 
